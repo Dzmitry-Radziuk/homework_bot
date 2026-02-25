43159f8e53806bd6338ac8ae8379b453cb338998
Dzmitry Radziuk <mitia.radiuk@yandex.ru>
2026-02-25 11:20:07 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -22,7 +22,7 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
+    'approved': 'Работа проверена',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
